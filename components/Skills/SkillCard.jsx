fad1c2d95e4557717cb6b051eb54de67b27beeba
Carson Haskell <carson.haskell@gmail.com>
2023-06-04 15:26:59 -0700

    fix skills layout for mobile

--- components/Skills/SkillCard.jsx
+++ components/Skills/SkillCard.jsx
@@ -1,6 +1,6 @@
 const SkillCard = ({ icon, siteUrl }) => {
   return (
-    <div className="xl:w-full w-[80%] p-10 m-auto lg:mb-4 duration-300 ease-in shadow-lg cursor-pointer rounded-[10%] hover:shadow-2xl hover:scale-105 lg:hover:scale-110">
+    <div className="w-[80%] p-10 m-auto lg:mb-4 duration-300 ease-in shadow-lg cursor-pointer rounded-[10%] hover:shadow-2xl hover:scale-105 lg:hover:scale-110">
       <div className="flex items-center justify-center">
         <a href={siteUrl} target="_blank" rel="noopener noreferrer">
           <div className="m-auto">{icon}</div>
--- components/Skills/Skills.jsx
+++ components/Skills/Skills.jsx
@@ -20,76 +20,78 @@ import { AzuresqldatabaseOriginal, PrismaOriginal } from "@beta/devicons-react";
 const SKILLS = [
   {
     name: "HTML",
-    icon: <Html5Original size="150" />,
+    icon: <Html5Original size="100" />,
     url: "https://developer.mozilla.org/en-US/docs/Glossary/HTML5#",
     id: 1,
   },
   {
     name: "CSS",
-    icon: <Css3Original size="150" />,
+    icon: <Css3Original size="100" />,
     url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
     id: 2,
   },
   {
     name: "JavaScript",
-    icon: <JavascriptOriginal size="150" />,
+    icon: <JavascriptOriginal size="100" />,
     url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     id: 3,
   },
   {
-    name: "Node.js",
-    icon: <NodejsOriginal size="150" />,
-    url: "https://nodejs.org/en/about",
-    id: 4,
-  },
-  {
     name: "React.js",
-    icon: <ReactOriginal size="150" />,
+    icon: <ReactOriginal size="100" />,
     url: "https://react.dev",
     id: 5,
   },
+
   {
     name: "Next.js",
-    icon: <NextjsOriginal size="150" />,
+    icon: <NextjsOriginal size="100" />,
     url: "https://nextjs.org",
     id: 6,
   },
   {
-    name: "Github",
-    icon: <GithubOriginal size="150" />,
-    url: "https://github.com/about",
-    id: 7,
+    name: "Redux",
+    icon: <ReduxOriginal size="100" />,
+    url: "https://react-redux.js.org",
+    id: 12,
   },
   {
     name: "Tailwind",
-    icon: <TailwindcssPlain size="150" />,
+    icon: <TailwindcssPlain size="100" />,
     url: "https://tailwindcss.com",
     id: 8,
   },
   {
+    name: "Github",
+    icon: <GithubOriginal size="100" />,
+    url: "https://github.com/about",
+    id: 7,
+  },
+  {
+    name: "Node.js",
+    icon: <NodejsOriginal size="100" />,
+    url: "https://nodejs.org/en/about",
+    id: 4,
+  },
+
+  {
     name: "MongoDB",
-    icon: <MongodbOriginal size="150" />,
+    icon: <MongodbOriginal size="100" />,
     url: "https://www.mongodb.com",
     id: 9,
   },
   {
     name: "SQL",
-    icon: <AzuresqldatabaseOriginal size="150" />,
+    icon: <AzuresqldatabaseOriginal size="100" />,
     url: "https://aws.amazon.com/what-is/sql/",
     id: 10,
   },
 
   {
     name: "Prisma",
-    icon: <PrismaOriginal size="150" />,
+    icon: <PrismaOriginal size="100" />,
     url: "https://www.prisma.io",
     id: 11,
-  },
-  {
-    name: "Redux",
-    icon: <ReduxOriginal size="150" />,
-    url: "https://react-redux.js.org",
-    id: 12,
   },
 ];
 
@@ -104,10 +106,10 @@ const Skills = () => {
           <h2 className="text-gray-700">What I Can Do</h2>
           <Lottie
             animationData={SkillAnim}
-            className="md:h-[50%] md:w-[50%] h-[80%] w-[80%] mb-16 lg:mb-0 lg:-mt-24 ease-in duration-500 hover:scale-105"
+            className="md:h-[50%] md:w-[50%] h-[80%] w-[80%] mb-16 lg:mb-0 lg:pb-0 lg:-mt-24 ease-in duration-500 hover:scale-105"
           />
         </div>
-        <div className="grid grid-cols-1 gap-4 px-2 xl:gap-12 xl:px-20 lg:gap-2 md:grid-cols-2 lg:grid-cols-4">
+        <div className="grid grid-cols-2 gap-4 px-2 xl:gap-12 md:gap-8 xl:px-36 lg:px-24 md:px-14 lg:gap-4 md:grid-cols-4">
           {SKILLS.map(skill => (
             <SkillCard key={skill.id} icon={skill.icon} siteUrl={skill.url} />
           ))}
